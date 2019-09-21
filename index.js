96310625450a8d0a1bddc61009d8ef6c4a6e4a8b
nbradf2 <nbradf2@gmail.com>
2019-09-21 14:57:18 -0500

    add scroll animation

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ $(window).scroll(function() {
         $('.animHead').removeClass('sticky');
     }
 });
+
+//Mobile Nav
+
+$('.mobileToggle').click(function() {
+    if ($('.animHead').hasClass('open-nav')) {
+        $('.animHead').removeClass('open-nav');
+    } else {
+        $('.animHead').addClass('open-nav');
+    }
+});
+
+$('.animHead li a').click(function() {
+    if ($('.animHead').hasClass('.open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.animHead').removeClass('open-nav');
+    }
+})
+
+// Nav Scroll
+
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $(html, body).animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+})
