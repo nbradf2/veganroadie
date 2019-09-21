00e339554547ac72c8a62da8b441f3286e2d2404
nbradf2 <nbradf2@gmail.com>
2019-09-21 14:45:47 -0500

    add sticky header js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Sticky Header
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+        $('.animHead').addClass('sticky');
+    } else {
+        $('.animHead').removeClass('sticky');
+    }
+});
